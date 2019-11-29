5be3db74eec12c9293cd565b2e83536247e6a82b
Neeraj-Palliyali <55308416+Neeraj-Palliyali@users.noreply.github.com>
2019-11-29 14:48:52 +0530

    email validation

--- lib/blocs/bloc.dart
+++ lib/blocs/bloc.dart
@@ -1,8 +1,24 @@
 import 'dart:async';
 
-class Bloc{
-  final _emailController = StreamController<String>();
-  final passwordController = StreamController<String>();
+import 'package:login_bloc/Validators.dart';
 
-  Function(String) get changeEmail => _emailController.sink.add;
+class Bloc extends Object with Validators{
+  final _email = StreamController<String>();
+  final _password = StreamController<String>();
+
+  Stream<String> get email => _email.stream.transform(validateEmail);
+
+  Stream<String> get password => _password.stream.transform(validatePassword);
+
+  Function(String) get changeEmail => _email.sink.add;
+
+  Function(String) get changePassword => _password.sink.add;
+
+
+  dispose(){
+    _email.close();
+    _password.close();
+  }
+
 }
+final bloc = Bloc();
