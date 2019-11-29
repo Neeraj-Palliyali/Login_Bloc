a7d1073ff32f0727a97e30ef04f44ba5fbaf1cb1
Neeraj-Palliyali <55308416+Neeraj-Palliyali@users.noreply.github.com>
2019-11-29 13:02:15 +0530

    Stream Controllder

--- lib/blocs/bloc.dart
+++ lib/blocs/bloc.dart
@@ -1,5 +1,8 @@
 import 'dart:async';
 
 class Bloc{
+  final _emailController = StreamController<String>();
+  final passwordController = StreamController<String>();
 
+  Function(String) get changeEmail => _emailController.sink.add;
 }
